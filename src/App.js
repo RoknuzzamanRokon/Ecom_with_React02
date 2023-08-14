8873c43547a77d9403f728cb70c6815b0fafa1d6
Rokon Ron <rokon.raz@gmail.com>
2023-08-14 19:31:08 +0600

    Add rouder link in product file.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { Container } from 'react-bootstrap';
+
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import HomeScreen from './screens/HomeScreen';
-
-
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
   return (
-    <div >
+    <Router >
       <Header/>
       <main className='py-2'>
         <Container>
-          <HomeScreen/>
-          
-        </Container>
-        
+          <Routes>
+            <Route path='/' Component={HomeScreen} exact />
+            <Route path='/product/:id' Component={ProductScreen} />
+          </Routes>
+        </Container>   
       </main>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
