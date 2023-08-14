aea930af327dc3e4bc78c2f5c916c80805375b10
Rokon Ron <rokon.raz@gmail.com>
2023-08-14 23:56:52 +0600

    Solve this error this stage.THIS stage working tree clen.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Container } from 'react-bootstrap';
 
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -14,7 +14,7 @@ function App() {
       <Header/>
       <main className='py-2'>
         <Container>
-          <Routes>
+         <Routes>
             <Route path='/' Component={HomeScreen} exact />
             <Route path='/product/:id' Component={ProductScreen} />
           </Routes>
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-function ProductScreen() {
+
+function ProductScreen({match}) {
+  // const productId = match.params.id;
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id);
+  console.log('product:', product);
   return (
-    <div>
-        Product
+    <div> 
+       {product.name}
     </div>
   )
 }
 
-export default ProductScreen
+
+export default ProductScreen;
+
