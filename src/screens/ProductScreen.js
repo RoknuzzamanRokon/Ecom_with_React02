8aa89e766f94a1120df782f6694daef0bf211474
Rokon Ron <rokon.raz@gmail.com>
2023-08-15 18:26:41 +0600

    Create a product home page.

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -12,8 +12,65 @@ function ProductScreen({match}) {
   console.log('product:', product);
   return (
     <div> 
-      <h2>{product.name}</h2>
-       
+      <Link to='/' className='btn btn-dark btn-sm rounded-3 my-3' style={{ width: '200px' }}>Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating value={product.rating} color={'#f8e825'}/>
+              {product.numReviews} Reviews
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+
+
+          </ListGroup>
+        </Col>
+         
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                  <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button className='btn-block' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>         
+        </Col>
+      </Row>
     </div>
   )
 }
